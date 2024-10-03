f71cf784d680b822265429345a34d57a453f1b1d
Bnks10061998 <62460395+Bnks10061998@users.noreply.github.com>
2024-10-03 15:05:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 //app.use(cors())
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://kaviyab-capstone-ft.netlify.app",
     methods:["GET","POST"]
   })
 );
